0f0ddcfd98bbf9f6b7eb9b005a0ca1757dde971e
Eoin Reilly <eoindreilly@gmail.com>
2019-09-10 21:57:44 +0100

    Added search bar, styling and aysnchronous data request to TheMovieDBApi

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import "./Search.css";
 import { List, Input, Select } from "antd";
+import Axios from "axios";
+import Title from "antd/lib/typography/Title";
+import { async } from "q";
 
 class Search extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            searchText: ""
+            searchText: "",
+            suggestions: []
         }
     }
 
@@ -17,6 +21,7 @@ class Search extends Component {
         return (
           <div>
             {this.calculatedDays()}
+            {this.searchBar()}
           </div>
         );
       }
@@ -40,6 +45,55 @@ class Search extends Component {
             </div>
           </div>
         );
+      }
+
+      /**
+       * Return the JSX for the search bar
+       */
+      searchBar() {
+        return (
+          <div className="search-fields-div">
+            <Input
+              placeholder="Type in a Tv show"
+              size="large"
+              className="search-input transparent-fields"
+              value={this.state.searchText}
+              onChange={e => this.handleSearchText(e)}
+            />
+          </div>
+        );
+      }
+
+      /**
+       * Asynchronously request data from TheMovieDB api
+       * @author eoin.reilly
+       */
+      handleSearchText = (e) => {
+        this.setState({
+          searchText: e.target.value
+        });
+        let words = e.target.value;
+        let suggestions = [];
+        const apiPath = `https://api.themoviedb.org/3/search/tv?api_key=de810845bc016371c4822a8a9550270d&language=en-US&query=${e.target.value}`;
+
+        if(words.length > 1) {
+          Axios.get(apiPath)
+            .then(response => {
+              response.data.results.map(
+                async result => {
+                  await suggestions.push({
+                    title: result.name,
+                    id: result.id
+                  })
+                }
+              );
+              this.setState({ suggestions })
+            })
+            .catch(function(error) {
+              //TODO: Error handling
+            })
+            .finally(function() {});
+        }
       }
       
     render() {
